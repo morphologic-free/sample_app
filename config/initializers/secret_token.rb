15dea9f624539cbc3a67e56b3da73cc41bcc696b
Zac Murray <zmurray@morphologic.com.au>
2012-05-28 21:45:10 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'fe22a86ea92e8a005f76f862fb53bc4aa1e505a41a8535ee5f5d7e46e3b2390d7b725070768595c0fd06b381062fbbe83b5f7797153e904ade19f2a088db86f5'
