451a9928a84f97ebc87f2fa1cfa5f2ff424ac7dd
Zac Murray <zmurray@morphologic.com.au>
2012-06-01 23:12:25 +1000

    user signup complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-  def gravatar_for(user, option = {:size => 50})
+  def gravatar_for(user, option = {:size => 50, :default => "monsterid"})
     gravatar_image_tag(user.email.downcase, :alt => user.name,
                                             :class => 'gravatar',
                                             :gravatar => option)
